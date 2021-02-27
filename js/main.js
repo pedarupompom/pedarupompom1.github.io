5c3ff573515d8c79a05a55b15f333a8db0a4178e
pedarupompom <79315760+pedarupompom@users.noreply.github.com>
2021-02-27 14:12:13 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}());
+(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn1');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}());
+
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn2');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}());
+
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn3');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}());
+
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn4');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}());
+
+(function () {
+  document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('dropdown__btn5');
+    if(btn) {
+      btn.addEventListener('click', function(){
+        this.classList.toggle('is-open');
+      });
+    }
+  });
+}());
